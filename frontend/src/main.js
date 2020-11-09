d0d14852bc2d6ebe62a69715f7c1b71766842d21
Matthew Hayward <mhayward@email.sc.edu>
2020-11-09 15:10:18 -0800

    Updating Home Page
    Not adding node_modules

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Navbar from './components/Navbar.vue';
+
+// line below is not working
+Vue.component('Navbar', require('./components/Navbar.vue').default);
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+      <Navbar></Navbar>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to the Colite Technologies Data Visualization Application"/>
   </div>
@@ -8,11 +9,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import Navbar from '@/components/Navbar.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
