063b761f0460a1987195312dfc7c8c05096cfcf4
Matthew Hayward <mhayward@email.sc.edu>
2020-11-09 15:10:32 -0800

    Update HelloWorld.vue
    Missed one

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,49 +1,55 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-       Colite Technologies assists businesses in switching to renewable energy sources to help
-      promote sustatinable business and energy resilience. Our application will allow for 
-      Colite Technologies to alter and input information into a database. This database will then
-      allow for users to view visualizations of the data the system receives from their systems.
-      The application has been implemented through Amazon Web Services.
-      <!-- <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>. -->
-    </p>
-    <!-- <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul> -->
+  <div>
+    <div>
+      <b-jumbotron>
+     <hr class="my-4" />       
+      </b-jumbotron>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card" style="margin = 20px">
+            <div class="card-body">
+              <h5 class="card-title">Learn about Colite Technologies</h5>
+              
+              <router-link to="/about">
+                <b-button size="lg" variant="primary">Our About Page</b-button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Need help with how to use the application?</h5>
+              <router-link to="/contact">
+                <b-button size="lg" variant="primary">
+                  Contact Us 
+                </b-button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./Navbar.vue";
+// import router from "../router.js";
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    Navbar
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {} 
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
