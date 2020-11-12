837991d8acaeba14cc00cb8fbce3c6ed394597df
Simon Benzer <simonhbenzer@gmail.com>
2020-11-12 16:54:16 -0500

    updated web app design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,12 +19,13 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #42b983;
+  background-color: #2c3e50;
+  
 }
 
 #nav {
   padding: 30px;
-  //background-color: #2c3e50;
   background-image: linear-gradient(to right, #2c3e50, #42b983);
 
   a {
