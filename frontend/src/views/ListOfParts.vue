d20764500c8fb1344f02bfe6058d80cfb2e18e8e
Simon Benzer <simonhbenzer@gmail.com>
2020-11-13 20:51:42 -0500

    incorporated the rest of the video:https://www.youtube.com/watch?v=CfyX_fg59m8 ... still not working though

--- frontend/src/views/ListOfParts.vue
+++ frontend/src/views/ListOfParts.vue
@@ -1,3 +1,5 @@
+<script src="vue.js"></script>
+
 <template>
   <div class="listOfParts">
     <h1>This is the parts list page</h1>
@@ -30,8 +32,86 @@
         <th>Date (M/D/Y)</th>
         <th>Time (H:M:S)</th>
       </tr>
-     <!-- update -->
-     <tr v-for='(user) in users' v-bind:key="user">
+      <!-- Add -->
+      <tr>
+        <td>
+          <input type="text" v-model='id_number'>
+        </td>
+        <td>
+          <input type="text" v-model='column_1'>
+        </td>
+        <td>
+          <input type="text" v-model='rpm'>
+        </td>
+        <td>
+          <input type="text" v-model='wind_v'>
+        </td>
+        <td>
+          <input type="text" v-model='wind_a'>
+        </td>
+        <td>
+          <input type="text" v-model='wind_a2'>
+        </td>
+        <td>
+          <input type="text" v-model='wind_a3'>
+        </td>
+        <td>
+          <input type="text" v-model='wind_w'>
+        </td>
+        <td>
+          <input type="text" v-model='pv_v'>
+        </td>
+        <td>
+          <input type="text" v-model='pv_a'>
+        </td>
+        <td>
+          <input type="text" v-model='untitled'>
+        </td>
+        <td>
+          <input type="text" v-model='at_v'>
+        </td>
+        <td>
+          <input type="text" v-model='bat_a'>
+        </td>
+        <td>
+          <input type="text" v-model='bat_w'>
+        </td>
+        <td>
+          <input type="text" v-model='led_1v'>
+        </td>
+        <td>
+          <input type="text" v-model='led_1a'>
+        </td>
+        <td>
+          <input type="text" v-model='led_1w'>
+        </td>
+        <td>
+          <input type="text" v-model='led_2v'>
+        </td>
+        <td>
+          <input type="text" v-model='led_2a'>
+        </td>
+        <td>
+          <input type="text" v-model='led_2w'>
+        </td>
+        <td>
+          <input type="text" v-model='temp_c'>
+        </td>
+        <td>
+          <input type="text" v-model='ex_in_v'>
+        </td>
+        <td>
+          <input type="text" v-model='date_mdy'>
+        </td>
+        <td>
+          <input type="text" v-model='time_hms'>
+        </td>
+        <td>
+        <input type="button" value='Add' @click='addRecord()'>
+        </td>
+    </tr>
+     <!-- update/delete -->
+     <tr v-for='(user,index) in users' v-bind:key="user">
       <td>
         <input type="text" v-model='user.id_number'>
       </td>
@@ -105,7 +185,8 @@
         <input type="text" v-model='user.time_hms'>
       </td>
       <td>
-      <input type="button" value='allRecords' @click='allRecords()'>
+      <input type="button" value='Update' @click='updateRecord(index,user.id_number)'>
+      <input type="button" value='Delete' @click='deleteRecord(index,user.id_number)'>
       </td>
     </tr>
     </table>
@@ -155,7 +236,96 @@
             .catch(function(error){
               console.log(error);
             });
+          },
+          addRecord: function(){
+            axios.post('ajaxfile.php',{
+              request: 2,
+              id_number: this.id_number,
+              column_1: this.column_1,
+              rpm: this.rpm,
+              wind_v: this.wind_v,
+              wind_a: this.wind_a,
+              wind_a2: this.wind_a2,
+              wind_a3: this.wind_a3,
+              wind_w: this.wind_w,
+              pv_v: this.pv_v,
+              pv_a: this.pv_a,
+              untitled: this.untitled,
+              bat_v: this.bat_v,
+              bat_a: this.bat_a,
+              bat_w: this.bat_w,
+              led_1v: this.led_1v,
+              led_1a: this.led_1a,
+              led_1w: this.led_1w,
+              led_2v: this.led_2v,
+              led_2a: this.led_2a,
+              led_2w: this.led_2w,
+              temp_c: this.temp_c,
+              ex_in_v: this.ex_in_v,
+              date_mdy: this.data_mdy,
+              time_hms: this.time_hms
+            })
+            .then(function(response){
+              app.allRecords();
+
+              app.id_number = "";
+              app.column_1 = "";
+              app.rpm = "";
+              app.wind_v = "";
+              app.wind_a = "";
+              app.wind_a2 = "";
+              app.wind_a3 = "";
+              app.wind_w = "";
+              app.pv_v = "";
+              app.pv_a = "";
+              app.untitled = "";
+              app.bat_v = "";
+              app.bat_a = "";
+              app.bat_w = "";
+              app.led_1v = "";
+              app.led_1a = "";
+              app.led_1w = "";
+              app.led_2v = "";
+              app.led_2a = "";
+              app.led_2w = "";
+              app.temp_c = "";
+              app.ex_in_v = "";
+              app.date_mdy = "";
+              app.time_hms = "";
+
+              alert(response.data);
+            })
+            .catch(function(error){
+              cosole.log(error);
+            })
           }
+        },
+        updateRecord: function(index,id){
+          var id_number = this.users[index].id_number;
+
+          axios.post('ajaxfile.php',{
+            request: 3,
+            id_number: id_number
+          })
+          .then(function(response){
+            alert(response.data);
+          })
+          .catch(function(error){
+            console.log(error);
+          })
+        },
+        deleteRecord: function(index,id_number){
+          axios.post('ajax.php',{
+            request: 4,
+            id_number: id_number
+          })
+          .then(function(response){
+            app.users.splice(index,1);
+            alert(response.data);
+          })
+          .catch(function(error){
+            console.log(error);
+          })
         }
       });
 </script>
--- frontend/src/views/config.php
+++ frontend/src/views/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "proof-of-concept-2.cg5oknnxxjpg.us-east-1.rds.amazonaws.com";
-$user = "root";
+$user = "admin";
 $password = "admin";
-$dbname = "proof-of-concept-2";
+$dbname = "proof_of_concept_mock_data_3";
 
 $con = mysqli_connect($host,$user,$password,$dbname);
 
