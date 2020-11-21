7cb9424edbbb5759158930a835901a3a605a65e5
Simon Benzer <simonhbenzer@gmail.com>
2020-11-20 20:05:09 -0500

    moved parts components' secondary navbar into listOfParts page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,18 +2,6 @@
 <template>
   <div id="app">
     <Navbar> </Navbar>
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">shbenzer</router-link>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/poles" class="nav-link">Poles</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/add" class="nav-link">Add</router-link>
-        </li>
-      </div>
-    </nav>
-
     <div class="container mt-3">
       <router-view />
     </div>
--- frontend/src/views/ListOfParts.vue
+++ frontend/src/views/ListOfParts.vue
@@ -3,6 +3,18 @@
 
 <template>
   <div id="listOfParts" class="listOfParts">
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand"></router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/poles" class="nav-link">List of Poles</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Add Poles</router-link>
+        </li>
+      </div>
+    </nav>
+    <br>
     <h1>Welcome to the list of all active parts page!</h1>
   </div>
 </template>
