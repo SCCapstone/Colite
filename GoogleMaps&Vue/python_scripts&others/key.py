eaedb404df3b48f901d802c54e44fc689e901508
knarla15 <45083716+knarla15@users.noreply.github.com>
2020-11-24 14:15:28 -0500

    //testing

--- GoogleMaps&Vue/python_scripts&others/key.py
+++
@@ -1,9 +0,0 @@
-try:
-	with open('../../.password/google-maps/api', 'r') as fp:
-		key = fp.readlines()
-
-	key = ''.join(key)
-
-except:
-	# Insert your API key here
-	key = 'AIzaSyD3lI3tWTw58b9iKKYgoX-qZpwZndHwsDc'
