ea906077ae54856da2d67785f060cb4c8b62ac68
Matthew Hayward <mhayward@email.sc.edu>
2021-03-17 14:34:37 -0400

    Update Navbar.vue
    Commenting out not working navbar parts

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -21,8 +21,8 @@
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input> -->
+          <!-- <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button> -->
         </b-nav-form>
 
         <!-- <b-nav-item-dropdown text="Lang" right>
@@ -35,10 +35,10 @@
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
           <template #button-content>
-            <em>User</em>
+            <!-- <em>User</em> -->
           </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          <!-- <b-dropdown-item href="#">Profile</b-dropdown-item> -->
+          <!-- <b-dropdown-item href="#">Sign Out</b-dropdown-item> -->
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
