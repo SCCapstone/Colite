d1f7879b8546b29e5ed75bc0a60bedd201444137
revt0 <49416643+revt0@users.noreply.github.com>
2021-04-06 19:39:42 -0400

    Using plotly for data visualization step 1
    Setting up javaScript usage in PlotlyGraphs

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -61,7 +61,7 @@ const routes = [{
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "PolesList" */ '../views/Graphs.vue')
+            import ( /* webpackChunkName: "Graphs" */ '../views/Graphs.vue')
     },
     {
         path: '/addPole',
--- frontend/src/views/Graphs.vue
+++ frontend/src/views/Graphs.vue
@@ -1,16 +1,57 @@
 <template>
-    <h1>
-        Welcome to the Graphs Page!
-    </h1>    
+    <div class="home">
+    
+      <h1 class="card-title">Welcome to the Colite Technology Data Visualization Application!</h1>
+      <div class="card-body" id="tester">
+        Test
+      </div>
+      <b-jumbotron> 
+      <div id="graph">
+          test3
+          {{ message }}
+        <!--<PlotlyTestv2></PlotlyTestv2>-->
+      </div>
+        </b-jumbotron> 
+    </div>
 </template>
 
 <script>
-// import statement
-// import Graphs from "../compenents/Graphs.vue";
+import * as mykey from '../components/PlotlyGraphs.js'
 
+export default {
+  data() {
+    return {
+      message: mykey.aVar // Hello Vue.js!
+    }
+  }
+}
+/*
+import  Plotly  from '../../node_modules/plotly';
 
-// export default {
-    
-
-// }
+export default {
+  name: "PlotlyTestv2",
+  template: '<div><h1> Hello </h1><div ref="plot1", id="plot1"></div></div>',
+  data: function() {
+    return {
+      count: 0,
+      data:[{ x: [1,2,3,4], y: [10,15,13,17], type:"scatter" }],
+      attr: { displayModeBar: true },
+      layout: { title: "My graph :)" }
+    };
+  },
+  mounted() {
+    Plotly.plot(this.$refs.plot1, this.data);
+  },
+  methods: {
+    button_click_func: function() {
+      console.log("Clicked the button: " + this.count, " y[0] = ", this.data[0].y[0]);
+      this.count += 1;
+      this.$set(this.data[0].y, 0, this.data[0].y[0] + 1);
+    }
+  } 
+}*/
 </script>
+
+<style>
+
+</style>
