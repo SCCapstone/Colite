3b2f63777c7275ee9473d85e99f06a27adcbc5ef
revt0 <49416643+revt0@users.noreply.github.com>
2021-04-08 17:59:23 -0400

    Major update to the chart
    now has the functionality to render data given by a javascript file.

--- frontend/src/components/PlotlyGraphs.js
+++ frontend/src/components/PlotlyGraphs.js
@@ -1,24 +1,63 @@
-import { Bar } from 'vue-chartjs'
+import { Line , mixins} from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
-  extends: Bar,
-  data: () => ({
+  extends: Line,
+  mixin: [reactiveProp],
+  props: {
     chartdata: {
-      labels: ['January', 'February'],
-      datasets: [
-        {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [40, 20]
-        }
-      ]
+      type: Object,
+      default: null
     },
     options: {
-      responsive: true,
-      maintainAspectRatio: false
+      type: Object,
+      default: null
     }
-  }),
+  },
+  methods: {
+    randomizeData: function() {
 
+      var newLabels = [];
+      var newData = [];
+      var i;
+
+      var len = 100;
+    
+      for(i = 0; i<len;++i) {
+        newLabels.push(i);
+      }
+
+      for(i =0; i<len;++i){
+        newData.push(Math.floor(Math.random()*100));
+      }
+      var aChartdata = {
+        labels: newLabels,
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: newData
+          }
+        ]
+      };
+      var aOptions = {
+        responsive: true,
+        maintainAspectRatio: false
+      
+      };
+      console.log("newlabels: "+newLabels);
+      console.log("new data: "+newData);
+
+
+
+
+      console.log("set data/options: "+this);
+      return aChartdata;
+    },
+    getRandomInt: function() {
+      return Math.sin(Math.random());
+    }
+  },
   mounted () {
     this.renderChart(this.chartdata, this.options)
   }
--- frontend/src/views/Graphs.vue
+++ frontend/src/views/Graphs.vue
@@ -1,4 +1,179 @@
-<!--<template>
+
+<template>
+	
+    <div class="dataVis">
+    
+      <h1 class="card-title">Welcome to the Colite Technology Data Visualization Application!</h1>
+      <b-jumbotron> 
+        <div >
+
+          
+          <button v-on:click="renderChart">Update</button>
+          <button v-on:click="rand">Randomize Data</button>
+
+        </div>
+
+      </b-jumbotron> 
+      <div class="small">
+        <line-chart :key="chartKey" 
+        :chartdata="dataLine" 
+        :options="options"></line-chart>
+
+      </div>
+    </div>
+
+</template>
+
+<script>
+import LineChart from '../components/PlotlyGraphs'
+
+export default({
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      chartKey: 0,
+      dataLine: null,
+      options: null
+    };
+  },
+  mounted () {
+    this.rand()
+  },
+  methods: {
+    renderChart: function() {
+      this.chartKey+=1;
+    },
+    rand: function() {
+      this.renderChart();
+      this.dataLine = LineChart.methods.randomizeData();
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false
+      
+      };
+    },
+    /*randomizeData: function() {
+
+      var newLabels = [];
+      var newData = [];
+      var i;
+
+      var len = 100;
+    
+      for(i = 0; i<len;++i) {
+        newLabels.push(i);
+      }
+
+      for(i =0; i<len;++i){
+        newData.push(Math.random(0,100));
+      }
+      var aChartdata = {
+        labels: newLabels,
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: newData
+          }
+        ]
+      };
+
+      console.log("setDataLine");
+      this.dataLine = aChartdata;
+      
+      
+      return;
+    },*/
+  }
+});
+</script>
+
+<style>
+
+</style>
+<!--
+export default {
+  components: {
+    LineChart
+  },
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+  mounted () {
+    
+    //this.fillData();
+  },
+  methods: {
+    fillData () {
+      
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
+}*/
+/*import * as graphScript from '../components/PlotlyGraphs.js'
+//var ctx = document.getElementById('myChart').getContext('2d');
+
+export default {
+  name: "default",
+  data() {
+    return {
+      grapher: graphScript.foo(4,5)
+    }
+  },
+  methods: {
+    w: function() {
+      graphScript.chartjs(ctx);
+      
+    }
+  }
+}
+/*
+import  Plotly  from '../../node_modules/plotly';
+
+export default {
+  name: "PlotlyTestv2",
+  template: '<div><h1> Hello </h1><div ref="plot1", id="plot1"></div></div>',
+  data: function() {
+    return {
+      count: 0,
+      data:[{ x: [1,2,3,4], y: [10,15,13,17], type:"scatter" }],
+      attr: { displayModeBar: true },
+      layout: { title: "My graph :)" }
+    };
+  },
+  mounted() {
+    Plotly.plot(this.$refs.plot1, this.data);
+  },
+  methods: {
+    button_click_func: function() {
+      console.log("Clicked the button: " + this.count, " y[0] = ", this.data[0].y[0]);
+      this.count += 1;
+      this.$set(this.data[0].y, 0, this.data[0].y[0] + 1);
+    }
+  } 
+}*/
+
+/*<template>
   <div id="app">
     <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
   </div>
@@ -44,135 +219,3 @@ export default {
 }
 </style>
 -->
-<template>
-	
-    <div class="home">
-    
-      <h1 class="card-title">Welcome to the Colite Technology Data Visualization Application!</h1>
-      <div class="card-body" id="tester">
-        {{ grapher }}
-        Plot:
-      </div>
-      <b-jumbotron> 
-      <div id="graph">
-          {{ grapher }}
-        
-        <button v-on:click="w">Button</button>
-      </div>
-
-        </b-jumbotron> 
-<div class="small">
-    <line-chart :chart-data="datacollection"></line-chart>
-
-  </div>
-    </div>
-
-</template>
-
-<script>
-import LineChart from '../components/PlotlyGraphs'
-/*
-export default {
-  components: {
-    LineChart
-  },
-  data () {
-    return {
-      datacollection: null
-    }
-  },
-  mounted () {
-    
-    //this.fillData();
-  },
-  methods: {
-    fillData () {
-      
-      this.datacollection = {
-        labels: [this.getRandomInt(), this.getRandomInt()],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [this.getRandomInt(), this.getRandomInt()]
-          }, {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [this.getRandomInt(), this.getRandomInt()]
-          }
-        ]
-      }
-    },
-    getRandomInt () {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-    }
-  }
-}*/
-export default({
-    components: {
-    LineChart
-  },
-  data: {
-    class1: 'revol',
-    class2: 'revol'
-  },
-  methods: {
-    rollDice: function() {
-      class1 += 'dic1';
-      class2 += 'dic2';
-      alert("confused");
-      // ... some code
-    },
-    w: function() {
-      alert("lll");
-    }
-  }
-});
-
-/*import * as graphScript from '../components/PlotlyGraphs.js'
-//var ctx = document.getElementById('myChart').getContext('2d');
-
-export default {
-  name: "default",
-  data() {
-    return {
-      grapher: graphScript.foo(4,5)
-    }
-  },
-  methods: {
-    w: function() {
-      graphScript.chartjs(ctx);
-      
-    }
-  }
-}
-/*
-import  Plotly  from '../../node_modules/plotly';
-
-export default {
-  name: "PlotlyTestv2",
-  template: '<div><h1> Hello </h1><div ref="plot1", id="plot1"></div></div>',
-  data: function() {
-    return {
-      count: 0,
-      data:[{ x: [1,2,3,4], y: [10,15,13,17], type:"scatter" }],
-      attr: { displayModeBar: true },
-      layout: { title: "My graph :)" }
-    };
-  },
-  mounted() {
-    Plotly.plot(this.$refs.plot1, this.data);
-  },
-  methods: {
-    button_click_func: function() {
-      console.log("Clicked the button: " + this.count, " y[0] = ", this.data[0].y[0]);
-      this.count += 1;
-      this.$set(this.data[0].y, 0, this.data[0].y[0] + 1);
-    }
-  } 
-}*/
-</script>
-
-<style>
-
-</style>
