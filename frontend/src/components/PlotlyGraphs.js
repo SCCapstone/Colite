8639d0110ce26861fef83fb7736db5d255aed363
revt0 <49416643+revt0@users.noreply.github.com>
2021-04-08 14:52:09 -0400

    trying vue-chartjs
    mock1 of vue-chartjs

--- frontend/src/components/PlotlyGraphs.js
+++ frontend/src/components/PlotlyGraphs.js
@@ -1,11 +1,29 @@
+import { Doughnut, mixins } from "vue-chartjs";
+export default {
+  extends: Doughnut,
+  props: ["data", "options"],
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.data, {
+      borderWidth: "10px",
+      hoverBackgroundColor: "red",
+      hoverBorderWidth: "10px"
+    });
+  }
+};
+
+/*import { LinearScale } from 'chart.js';
+import Vue from 'vue';
+import { Line } from 'vue-chartjs'
 
 var plotly = require('plotly/index.js')('node-test-account', 'tpmz9ye8hg');
 export const aVar = 'Vue';
 
-export function chartjs (){
-var ctx = document.getElementById('myChart').getContext('2d');
+/*
+function chartjs (ctx){
 
-var myChart = new Chart("myChart", {
+var myChart = new Chart(ctx, {
 
     type: 'bar',
     data: {
@@ -40,7 +58,7 @@ var myChart = new Chart("myChart", {
         }
     }
 });
-
+return myChart;
 }
 
 export function graphed(dataX, dataY) {
@@ -78,3 +96,75 @@ export function layout() {
     return layout1;
 }
 
+/*
+export default({
+    extends: Line,
+    props: ['chartdata','options'],
+    data() {
+        return {
+            class1: 'revol',
+            class2: 'revol'
+        }
+
+    },
+    methods: {
+      rollDice: function() {
+        class1 += 'dic1';
+        class2 += 'dic2';
+        alert("confused");
+        // ... some code
+      },
+      w: function() {
+        alert("lll");
+      },
+      test: function() {
+        
+        
+      },
+      
+      /*chartjs: function  (){
+        
+        var myChart = new Chart(class1, {
+    
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+        });
+        return myChart;
+    }
+    
+    },
+    //End Methods
+    mounted() {
+        this.renderChart(this.chartdata,this.options)
+    }
+  });*/
--- frontend/src/views/Graphs.vue
+++ frontend/src/views/Graphs.vue
@@ -1,5 +1,50 @@
-
 <template>
+  <div id="app">
+    <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+  </div>
+</template>
+
+<script>
+import PieChart from "../components/PlotlyGraphs";
+export default {
+  name: "App",
+  components: {
+    PieChart
+  },
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: ["Green", "Red", "Blue"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5]
+          }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<!--<template>
 	
     <div class="home">
     
@@ -11,19 +56,80 @@
       <b-jumbotron> 
       <div id="graph">
           {{ grapher }}
-        <!--<PlotlyTestv2></PlotlyTestv2>-->
+        
         <button v-on:click="w">Button</button>
       </div>
-      <div id=myChart> </div>
-        </b-jumbotron> 
-        <!--<canvas id="myChart" width="400" height="400"></canvas>-->
 
+        </b-jumbotron> 
+<div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
     </div>
 
 </template>
 
 <script>
-import * as graphScript from '../components/PlotlyGraphs.js'
+import LineChart from '../components/PlotlyGraphs'
+
+export default {
+  components: {
+    LineChart
+  },
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+  mounted () {
+    
+    this.fillData();
+  },
+  methods: {
+    fillData () {
+      
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
+}*/
+/*export default({
+  
+  data: {
+    class1: 'revol',
+    class2: 'revol'
+  },
+  methods: {
+    rollDice: function() {
+      class1 += 'dic1';
+      class2 += 'dic2';
+      alert("confused");
+      // ... some code
+    },
+    w: function() {
+      alert("lll");
+    }
+  }
+});
+
+/*import * as graphScript from '../components/PlotlyGraphs.js'
+//var ctx = document.getElementById('myChart').getContext('2d');
+
 export default {
   name: "default",
   data() {
@@ -33,11 +139,11 @@ export default {
   },
   methods: {
     w: function() {
-      graphScript.chartjs();
+      graphScript.chartjs(ctx);
       
     }
   }
-}
+}*/
 /*
 import  Plotly  from '../../node_modules/plotly';
 
@@ -67,4 +173,4 @@ export default {
 
 <style>
 
-</style>
+</style>-->
