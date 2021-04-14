98a718b0b362d04f4cae8d45060fda4ba312f7c3
Matthew Hayward <mhayward@email.sc.edu>
2021-04-13 20:29:10 -0400

    Progress

--- frontend/src/views/GraphsNew.vue
+++ frontend/src/views/GraphsNew.vue
@@ -25,23 +25,80 @@
 
 <script>
 import LineChart from '../components/ChartsGraphData.vue'
+import PoleDataService from "../services/PoleDataService";
 
 export default {
   name: 'LineChartContainer',
   components: { LineChart },
-  data: () => ({
+  data: () => {
+    return {
     loaded: false,
-    chartdata: null
-  }),
-  async mounted () {
-    this.loaded = false
-    try {
-      const { userlist } = await fetch('/api/userlist')
-      this.chartdata = userlist
-      this.loaded = true
-    } catch (e) {
-      console.error(e)
+    chartdata: null,
+    poles: [],
+    currentPoll: null,
+    currentIndex: -1,
+    pole_id: ""
+  };
+},
+  methods: {
+    retrievePoles() {
+      PoleDataService.getAll()
+        .then(response => {
+          this.poles = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    refreshList() {
+      this.retrievePoles();
+      this.currentPole = null;
+      this.currentIndex = -1;
+    },
+
+    setActivePole(pole, index) {
+      this.currentPole = pole;
+      this.currentIndex = index;
+    },
+
+    removeAllPoles() {
+      PoleDataService.deleteAll()
+        .then(response => {
+          console.log(response.data);
+          this.refreshList();
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    
+    searchIdNumber() {
+      PoleDataService.findByIdNumber(this.pole_id)
+        .then(response => {
+          this.poles = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    redFlag(pole) {
+      if (pole.ex_in_v == 3.4) {
+        document.getElementById("exinv").style.color = "red";
+        document.getElementById("idnumber").style.color = "red";
+      }
+      else{
+        document.getElementById("exinv").style.color = "#95c23b";
+        document.getElementById("idnumber").style.color = "#95c23b";
+      }
     }
+  },
+  mounted() {
+    this.retrievePoles();
+    //this.redFlag(pole);
   }
-}
+};
 </script>
