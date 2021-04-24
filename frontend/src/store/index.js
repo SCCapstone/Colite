aad692b80f47ab9f746e565666ccb07b4984c976
knarla15 <45083716+knarla15@users.noreply.github.com>
2021-04-24 15:26:32 -0400

    commenting code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,6 +3,8 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+//Due to using a single state tree, all states of application are contained inside one big object. However, as our application grows in scale, the store can get really bloated.
+//To help with that, Vuex allows us to divide our store into modules.
 export default new Vuex.Store({
   state: {
   },
