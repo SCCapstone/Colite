616786e12b24c03db13594159d70bc0fe3accf16
knarla15 <45083716+knarla15@users.noreply.github.com>
2021-04-24 15:22:44 -0400

    Update vue.config.js
    commenting code

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    //To make lint errors show up in the browser overlay, you can use lintOnSave: 'default'-> we have it set to false and therefore they will not show up
     lintOnSave: false,
     devServer: {
         port: 8081
